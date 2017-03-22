aeb0b54dfd0cea734c0ddbbe3c6398915e45eac9
Runnan Zhong <r.tina.zhong@gmail.com>
2017-03-22 19:23:10 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,6 @@ class Student
   end
 
   def self.create_table
-
     sql = <<~SQL
     CREATE TABLE IF NOT EXISTS students(
       id INTEGER PRIMARY KEY,
@@ -47,7 +46,6 @@ class Student
     new_student = self.new(name, grade)
     new_student.save
     new_student
-
   end
 
   # Remember, you can access your database connection anywhere in this class
