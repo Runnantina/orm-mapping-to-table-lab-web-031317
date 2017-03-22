bd8f2e21d414cc9ccbe131babe051b883ef96aa3
Runnan Zhong <r.tina.zhong@gmail.com>
2017-03-22 19:05:09 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ Bundler.require
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+# create a table with the same name as our class
+# column names then EQUALS to the attr_accessors
